df4e18248d92ad659b86b0246dd59d24b315b6c3
Noe De La Mora <noedelamora@gmail.com>
2019-05-23 00:07:07 -0500

    changing to postgres db

--- neopetsproject/neopetsproject/settings.py
+++ neopetsproject/neopetsproject/settings.py
@@ -87,13 +87,9 @@ WSGI_APPLICATION = 'neopetsproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))}
 
 
 # Password validation
