5220cf1bbba1d78405c7b2bf86caf93e83594432
Noe De La Mora <noedelamora@gmail.com>
2019-05-24 19:16:43 -0500

    Update local_settings.example.py

--- neopetsproject/local_settings.example.py
+++ neopetsproject/local_settings.example.py
@@ -7,10 +7,15 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 os.environ['SECRET_KEY'] = '{project secret key}'
 
+# Database
+os.environ['DATABASE_URL'] = 'postgres://url'
+
 # pet finder API variables
 os.environ['PETFINDER_BASE_URL'] = 'https://api.petfinder.com'
 os.environ['PETFINDER_API_KEY'] = '{client id}'
 os.environ['PETFINDER_API_SECRET'] = '{client secret}'
 os.environ['PETFINDER_TOKEN_TYPE'] = 'Bearer'
-os.environ['PETFINDER_LAST_REQUEST_TIME'] = ''
 os.environ['PETFINDER_ACCESS_TOKEN'] = ''
+
+# google API
+os.environ['GOOGLE_MAPS_API_KEY'] = '{api key}'
